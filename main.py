81d6e56b04ffb498153b4b324473118b17b89dab
Stéphane Van Erp <stephane.vanerp@unamur.be>
2021-01-04 20:44:40 +0100

    [Docker] Indiquer hote Flask à 0.0.0.0 au lieu de localhost

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from application import init_app
 app = init_app()
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
